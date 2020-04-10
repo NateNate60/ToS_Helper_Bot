8d55c915f91ecb502ae551ec51928c055ed0af51
Nathan Lim <41277019+NateNate60@users.noreply.github.com>
2020-04-10 11:06:49 -0700

    Add files via upload

--- ToS Definition Bot.py
+++ ToS Definition Bot.py
@@ -53,6 +53,7 @@ def get_time() :
     st = datetime.datetime.fromtimestamp(st).strftime('%Y-%m-%d %H:%M:%S')
     return st
 
+# A list of triggers for the bot.
 def check_triggers(chknum, time, c, b) :
     # Here, we use the well-established and respected technique of chaining together millions of if statements to simulate artificial inteligence.
     # Seriously, we just use if statements to check if any triggers are in the comment.
@@ -61,15 +62,43 @@ def check_triggers(chknum, time, c, b) :
     # c is the comment itself.
     
     # Use if, not elif, because we want the bot to be able to trigger more than once.
-    if "vfr" in b :
-        print (time + ": " + c.author.name + " queried VFR.")
+    
+    
+    # THESE TRIGGERS IN THIS FIRST IF STATEMENT WILL GO ONLY IF SUMMONED.
+    if "what is" in b or "what's" in b or "!def" in b :
+        if "vfr" in b :
+            print (time + ": " + c.author.name + " queried VFR.")
+            
+            #I've borrowed Seth's language here.
+            c.reply("VFR stands for Voting For Roles. It is the act of voting someone up to the stand to get a role claim from them." +
+                    "This helps narrow down the list of roles that remain in the game and generally helps the Town a lot more than evils." +
+                    signature)
+            crt = write_comment_list(c.id, crt)
+    
+    
+    
+    #ANYTHING BELOW THIS LINE WILL TRIGGER REGARDLESS OF WHETHER THE BOT WAS INTENTIONALLY SUMMONED OR NOT
+    
+    # This rather long line checks for "new" and "player" in the title, OR "noob" and something else that says the OP is the noob.
+    if "new" in c.name.lower() and "player" in c.name.lower() or ("noob" in c.name.lower() and ("player" in c.name.lower() or "here" in c.name.lower() or "'m" in c.name.lower() or "im" in c.name.lower() ) ):
+        print (time + ": " + c.author.name + " queried new player.")
         
-        #I've borrowed Seth's language here.
-        c.reply("VFR stands for Voting For Roles. It is the act of voting someone up to the stand to get a role claim from them." +
-                "This helps narrow down the list of roles that remain in the game and generally helps the Town a lot more than evils." +
-                signature)
+        # Also borrowed from Seth
+        c.reply("It seems like you're a new player to the game." + ' [Here is an extremely helpful guide made by /u/NateNate60, one of the Moderators ' +
+                "for this subreddit](https://drive.google.com/file/d/1TC_hue8fEqH3xas2yMVU8KqQA-MovRZ-/view?usp=drivesdk), and another guide made "
+                'by Chancell0r on the Forums; https://blankmediagames.com/phpbb/viewtopic.php?f=3&t=73489&p=2399389. You should also check the sidebar' +
+                ' of the subreddit for any other useful links, such as the [Frequently Asked Questions](https://www.reddit.com/r/TownofSalemgame/wiki/faq) ' +
+                'and the ["Is is against the rules?"](https://www.redd.it/fucmif?sort=qa) thread.' + signature)
         crt = write_comment_list(c.id, crt)
-
+    if "freez" in c.name.lower() or "lag" in c.name.lower() or "disconnect" in c.name.lower() or "dc" in c.name.lower() and c.link_flair_text.strip().lower() == 'question' :
+        print (time + ": " + c.author.name + "queried for freezing and lagging.")
+        c.reply("If your game siezes up and stops responding, try one of the following fixes. \n\n" +
+                "ON BROWSER: Try resizing the browser window a few times. Nobody is quite sure why this works, but it occassionally fixes connection issues and visual glitches. It may have to do with forcing the game to redraw." +
+                "\n\n ON STEAM: Try verifying the game integrity. You can do this by going into your Steam library, then going into the game's Properties, then Local Files, then clicking the Verify Integrity button. You may also try resizing the game's window when this issue occurs." +
+                "\n\n ON MOBILE: Try restarting your phone or re-installing the app." +
+                "\n\n IN GENERAL: Wired connections are always going to be more stable than wireless ones. If possible, use a wired Ethernet connection where possible to prevent packet loss, which is often what causes disconnection issues." +
+                "The game doesn't deal with packet loss that well. This can occassionally happen even on strong Wi-Fi or cellular connections." + signature)
+        crt = write_comment_list(c.id, crt)
 
 # And now, the meat of the bot.
 def run_bot() : # Add required variables later when I need them. I can't remember them all right now.
@@ -89,9 +118,14 @@ def run_bot() : # Add required variables later when I need them. I can't remembe
         # We also don't want to try to reply to locked/archived comments because that will throw an error.
         # We will also check that the user actually wants a definition so the bot doesn't reply to everyone who mentions VFR.
         # Yes, this if statement is long, but that's to prevent the stupidly indented body of code that happened in SUEB because Python enforces indentation.
-        if c.author not in config.banned and c.id not in crt and c.locked == False and c.archived == False and ("what is" in b or "what's" in b or "!def" in b): 
+        if c.author not in config.banned and c.id not in crt and c.locked == False and c.archived == False : 
             check_triggers(chknum, time, c, b)
     
+    # Same thing as above, but checks posts instead of comments.
+    for post in r.subreddit('TownofSalemgame').new(limit=chknum) :
+        b = comment.body.lower()
+        if c.author not in config.banned and c.id not in crt and c.locked == False and c.archived == False and ("what is" in b or "what's" in b or "!def" in b): 
+            check_triggers(chknum, time, c, b)
     # Check the inbox for any direct requests (people summoning the bot by pinging it).
     for message in r.inbox.mentions() :
         # Check only unread mentions. That way, we don't have to keep track of what we've already checked. Reddit will do that for us.
@@ -109,7 +143,7 @@ def run_bot() : # Add required variables later when I need them. I can't remembe
             # The bot will not check its own comments for triggers.
             elif message.parent().author.name != r.user.me().name :
                 # Check the parent comment for triggers.
-                check_triggers(chknum, time, message.parent(), ,mesage.parent().body.lower())
+                check_triggers(chknum, time, message.parent(), mesage.parent().body.lower())
             
             # REGARDLESS of whether we found any triggers or now, the message will be marked as read so we don't check it again.
             message.mark_read()
