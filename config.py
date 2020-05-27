acb2ed20ae02859bdd4fc2149fd068a468211437
Nathan Lim <41277019+NateNate60@users.noreply.github.com>
2020-05-26 17:44:49 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 # WELCOME TO THE TOS_DEFINITION_BOT CONFIG FILE!
 
+
+# ARE YOU USING THE SEPARATE AUTH.PY FILE? IF YES, UNCOMMENT LINES 48, 44, 40, 36, 31, and 5
+#import auth
+
+
+
+
+
+
 # How many posts should a user be able to make in a single day before they're warned about flooding?
-max_posts = 10
+max_posts = 999
 
 # Who should not be able to make any requests with the bot?
 blacklisted = []
@@ -15,22 +24,24 @@ tick = 0
 
 # What should the bot sign its messages with?
 signature = "\n\n I try my best to be accurate and trigger on only those who need it, but I do make mistakes. Report" \
-            " mistakes to NateNate60 or the mods. \n\n ^NateNate60's ^ToS\\_Helper\\_Bot"
+            " mistakes to u/NateNate60 or the mods. \n\n [About ToS Helper Bot](https://natenate60.xyz/tos-helper-bot)"
 
 # What's the username the bot should sign in with?
 username = "ToS_Helper_Bot"
+#username = auth.username
+
 
 # What's the account's password?
-password = "NICE TRY LOL I'M KEEPING THIS SECRET FOR NOW"
+password = "password"
+#password = auth.password
 
 # What's the bot's client ID?
-client_id = "An-f7DowBxxuVw"
+client_id = "client_id"
+#client_id = auth.client.id
 
 # What's the client_secret?
-client_secret = "It's important that I keep this a... secret. Because it's the client_secret, obviously."
+client_secret = "client_secret"
+#client_secret = auth.client_secret
 
-# Where is the bot installed to? Where did you git clone the repo to?
-# This is needed because to run the bot as a service in systemd, absolute file paths are required.
-# If you neglect to change this, FileNotFoundError will be raised and the bot will fail.
-# YES THE SLASH AT THE END IS NEEDED. IT DOESN'T WORK WITHOUT IT
-workingdir = '/thb/'
+#THE SLASH IS NECESSARY
+workingdir="/thb/"
