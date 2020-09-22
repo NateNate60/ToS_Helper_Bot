bcbfbcbf7453b7f6ba115efe8956f2a6404b80d7
Nathan Lim <41277019+NateNate60@users.noreply.github.com>
2020-09-22 11:34:08 -0700

    Removed log to file
    It doesn't work. Line 360 raises SyntaxError

--- ToS_Helper_Bot.py
+++ ToS_Helper_Bot.py
@@ -356,11 +356,14 @@ def log(*msg, **kwargs):
     :param msg: The message to log.
     :return: None.
     """
+    """
     logoutput = datetime.datetime.fromtimestamp(time.time()).strftime('[%Y-%m-%d %H:%M:%S]') + ' ' + *msg + ' ' + **kwargs
     print(logoutput)
     if settings.logtofile :
         with open ('log.txt', 'a') as l :
             l.write('\n' + logoutput)
+    """
+    print (datetime.datetime.fromtimestamp(time.time()).strftime('[%Y-%m-%d %H:%M:%S]') + ' ' + *msg + ' ' + **kwargs)
 
 if not path.exists ('log.txt')
     with open ('log.txt','w') as l :
