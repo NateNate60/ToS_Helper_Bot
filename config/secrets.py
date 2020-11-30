24972ff875b7af804b18a8bc375f6e8427f2971d
Nathan Lim <41277019+NateNate60@users.noreply.github.com>
2020-11-30 09:00:58 -0800

    Create a sample secrets file

---
+++ config/secrets.py
@@ -0,0 +1,13 @@
+# SECRETS FILE
+# All secrets, such as passwords and tokens, go here.
+
+#Reddit username and password
+username = "ToS_Helper_Bot"
+password = "password"
+
+#Reddit API credentials
+client_id = "id"
+client_secret = "secret"
+
+#Discord token (for TrialBot features)
+token = "token"
