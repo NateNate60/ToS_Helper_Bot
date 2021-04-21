0f04a61ddc3de212affcd3403eccdab0898bfa72
NateNate60 <nathanlim2004@gmail.com>
2021-04-21 05:58:30 +0000

    Removed unused comment
    This will never work

--- tlogging.py
+++ tlogging.py
@@ -6,11 +6,4 @@ def log(*msg, **kwargs):
     :param msg: The message to log.
     :return: None.
     """
-    """
-    logoutput = datetime.datetime.fromtimestamp(time.time()).strftime('[%Y-%m-%d %H:%M:%S]') + ' ' + *msg + ' ' + **kwargs
-    print(logoutput)
-    if settings.logtofile :
-        with open ('log.txt', 'a') as l :
-            l.write('\n' + logoutput)
-    """
     print (datetime.datetime.fromtimestamp(time.time()).strftime('[%Y-%m-%d %H:%M:%S]'), *msg, **kwargs)
